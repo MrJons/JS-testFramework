e18c5cb267f695258404321d472cfe654583b719
MrJons <shad_stick@hotmail.co.uk>
2016-09-26 12:50:13 +0100

    added contains test and checks for failing tests.

--- spec/exampleTestSpec.js
+++ spec/exampleTestSpec.js
@@ -1,3 +1,16 @@
 
 
 equal(5,5);
+equal(5,10);
+
+notEqual(5,6);
+notEqual(5,5);
+
+contains('string', 'i');
+contains('string', 'x');
+
+contains([1,2,3], 2);
+contains([1,2,3], 4);
+
+notContains('string', 'x');
+notContains('string', 'i');
