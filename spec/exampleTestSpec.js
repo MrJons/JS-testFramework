54560c0d032b4c781eccb01a70107060dae80317
TomStuart92 <Thomascstuart@gmail.com>
2016-09-26 13:09:01 +0100

    Add It/Describes blocks

--- spec/exampleTestSpec.js
+++ spec/exampleTestSpec.js
@@ -1,16 +1,26 @@
+describe('custom testing framework', function(){
+  it('has a equal matcher', function(){
+    expect(equal(5,5));
+    expect(equal(5,10));
+  });
 
+  it('has a notEqual matcher', function(){
+    expect(notEqual(5,6));
+    expect(notEqual(5,5));
+  });
 
-equal(5,5);
-equal(5,10);
+  it('has a string contains matcher', function(){
+    expect(contains('string', 'i'));
+    expect(contains('string', 'x'));
+  });
 
-notEqual(5,6);
-notEqual(5,5);
+  it('has a array contains matcher', function(){
+    expect(contains([1,2,3], 2));
+    expect(contains([1,2,3], 4));
+  });
 
-contains('string', 'i');
-contains('string', 'x');
-
-contains([1,2,3], 2);
-contains([1,2,3], 4);
-
-notContains('string', 'x');
-notContains('string', 'i');
+  it('has a notContains matcher', function(){
+    expect(notContains('string', 'x'));
+    expect(notContains('string', 'i'));
+  });
+});
